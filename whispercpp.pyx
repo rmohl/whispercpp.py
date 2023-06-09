4c874d9d0ed313cce7e87749b2c49b408da3b2c3
Rachael <48801105+rmohl@users.noreply.github.com>
2023-06-09 13:45:22 -0400

    trying to make python function for returning params

--- whispercpp.pyx
+++ whispercpp.pyx
@@ -130,6 +130,10 @@ cdef class Whisper:
     
     def get_params(self):
         params = {}
+        prompt_tokens = []
+        
+#         for token in self.params.prompt_tokens:
+            
         
         params.update({
             "n_threads": self.params.n_threads,
@@ -150,7 +154,7 @@ cdef class Whisper:
             "max_tokens": self.params.max_tokens,
             "speed_up": self.params.speed_up,
             "audio_ctx": self.params.audio_ctx,
-            "prompt_tokens": self.params.prompt_tokens,
+#             "prompt_tokens": self.params.prompt_tokens,
             "prompt_n_tokens": self.params.prompt_n_tokens,
             "language": self.params.language
         })
